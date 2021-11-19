ebad70fa5ddfb7f5866f962d9cb88c659c971197
layne <layne@atguigu.com>
2021-11-19 23:06:25 +0800

    master commit-1

--- cloud-consumer-order-80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order-80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
-        System.out.println("修改一点点代码");
-        System.out.println("push text!");
     }
 }
