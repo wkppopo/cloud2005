03d63e52bb2bb46b41e70930eeff84ad00f75433
layne <layne@atguigu.com>
2021-11-19 21:27:12 +0800

    cloud2005 second

--- cloud-consumer-order-80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order-80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
+        System.out.println("修改一点点代码");
     }
 }
